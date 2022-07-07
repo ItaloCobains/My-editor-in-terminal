81d655fab1f869f368212e0d641b695d7de75503
Italo <italobrandao.contato@gmail.com>
2022-07-07 09:06:04 -0300

    fix arrow

--- kilo.c
+++ kilo.c
@@ -378,6 +378,11 @@ void editorMoveCursor(int key)
     {
       E.cx--;
     }
+    else if (E.cy > 0)
+    {
+      E.cy--;
+      E.cx = E.row[E.cy].size;
+    }
     break;
   case ARROW_RIGHT:
     if (row && E.cx < row->size)
