fac03f96858692ecba102186c38be48c675d4b3b
Italo <italobrandao.contato@gmail.com>
2022-07-14 09:03:07 -0300

    set init vars and editorconfig

--- kilo.c
+++ kilo.c
@@ -45,6 +45,7 @@ typedef struct erow
 struct editorConfig
 {
   int cx, cy;
+  int rx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -218,6 +219,18 @@ int getWindowSize(int *rows, int *cols)
 
 /*** row operations ***/
 
+int editorRowCxtoRx(erow *row, int cx){
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++)
+  {
+    if(row->chars[j] == '\t')
+        rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP)
+    rx++;
+  }
+  return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
   int tabs = 0;
@@ -318,6 +331,8 @@ void abFree(struct abuf *ab)
 
 void editorScroll()
 {
+  E.rx = E.cx;
+
   if (E.cy < E.rowoff)
   {
     E.rowoff = E.cy;
@@ -326,13 +341,13 @@ void editorScroll()
   {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.coloff)
+  if (E.rx < E.coloff)
   {
-    E.coloff = E.cx;
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols)
+  if (E.rx >= E.coloff + E.screencols)
   {
-    E.coloff = E.cx - E.screencols + 1;
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -396,7 +411,7 @@ void editorRefreshScreen()
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -499,6 +514,7 @@ void initEditor()
 {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
